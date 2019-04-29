de3380fa1281967fb6b20a8dda9c35e89340c95b
Michal-Szczepaniak <m.szczepaniak.000@gmail.com>
2019-04-29 12:59:58 +0200

    Enable sf hack

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -45,5 +45,8 @@ TARGET_SYSTEM_PROP += $(DEVICE_PATH)/system.prop
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.qcom
 
+# SurfaceFlinger init
+BOARD_USE_MOTO_SF = true
+
 # inherit from the proprietary version
 -include vendor/motorola/titan/BoardConfigVendor.mk
